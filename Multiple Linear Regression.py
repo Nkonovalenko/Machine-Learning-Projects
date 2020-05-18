9ea5c4530bec9dc21537b9232c55b30b06bcfdf5
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2020-05-18 12:21:40 -0400

    Reading in data

--- Multiple Linear Regression.py
+++ Multiple Linear Regression.py
@@ -3,3 +3,20 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 %matplotlib inline
+
+# Read in the data
+df = pd.read_csv("FuelConsumption.csv")
+
+# Loop at the data set
+df.head()
+
+# Set up categories
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+
+# Plot emission values with respect to Engine size:
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS, color='blue')
+plt.xlabel("Engine Size")
+plt.ylabel("Emission")
+plt.show()
+
