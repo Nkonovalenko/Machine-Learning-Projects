8079f7068fc839bbed6d21702a542530887a5d72
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2020-05-19 14:19:32 -0400

    Plot the data

--- Non-Linear Regression.py
+++ Non-Linear Regression.py
@@ -1,3 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as numpy
+import pandas as pd
 #%matplotlib inline			# This line is needed for Jupyter Notebook
+
+# Read in the data
+df = pd.read_csv("china_gdp.csv")
+df.head(10)
+
+# Plot the data set
+plt.figure(figsize=(8,5))
+x_data, y_data = (df["Year"].values, df["Value"].values)
+plt.plot(x_data, y_data, 'ro')
+plt.ylabel('GDP')
+plt.xlabel('Year')
+plt.title("Data Plot")
+
+plt.show()
