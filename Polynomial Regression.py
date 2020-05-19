84b98f3f3004942bff505ec28a2f48f8779a67cc
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2020-05-19 13:07:58 -0400

    Read in Data

--- Polynomial Regression.py
+++ Polynomial Regression.py
@@ -3,3 +3,9 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 #%matplotlib inline       # This line is needed for Jupyter Notebook
+
+# Read in the data
+df = pd.read_csv("FuelConsumption.csv")
+
+# look at dataset
+df.head()
