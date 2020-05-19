f993ecf0f709cc22f23e4bad44fb7f272157754d
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2020-05-19 14:04:35 -0400

    Added comments

--- Multiple Linear Regression/Multiple Linear Regression.py
+++ Multiple Linear Regression/Multiple Linear Regression.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 from sklearn import linear_model
-#%matplotlib inline
+#%matplotlib inline				# This line is needed for Jupyter Notebook
 
 # Read in the data
 df = pd.read_csv("FuelConsumption.csv")
